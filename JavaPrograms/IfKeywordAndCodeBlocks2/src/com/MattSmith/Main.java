d90259d75618f1a9eba48366659b8b965f6cd3e0
Matthew Smith <masmith.mis@gmail.com>
2018-01-02 17:03:20 -0600

    1/2/2018

--- JavaPrograms/IfKeywordAndCodeBlocks2/src/com/MattSmith/Main.java
+++ JavaPrograms/IfKeywordAndCodeBlocks2/src/com/MattSmith/Main.java
@@ -3,6 +3,32 @@ package com.MattSmith;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if(score < 5000 && score > 1000) {
+//            System.out.println("Your score is less than 5000. But, greater than 1000. Your score is " + score);
+//        } else if (score < 1000){
+//            System.out.println("Your score was less than 1000. Your score is " + score);
+//        } else {
+//            System.out.println("Got here. You score is " + score);
+//        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
---
+++ JavaPrograms/Methods2/src/com/MattSmith/Main.java
@@ -0,0 +1,62 @@
+package com.MattSmith;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+
+        displayHighScorePosition("Matt", calculateHighScorePosition(1500));
+        displayHighScorePosition("Jason", calculateHighScorePosition(900));
+        displayHighScorePosition("Tim", calculateHighScorePosition(400));
+        displayHighScorePosition("Brent", calculateHighScorePosition(50));
+        displayHighScorePosition("Stef", calculateHighScorePosition(1000));
+        displayHighScorePosition("Connie", calculateHighScorePosition(500));
+        displayHighScorePosition("Madi", calculateHighScorePosition(100));
+        displayHighScorePosition("Louise", calculateHighScorePosition(50));
+
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition
+                + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            return  finalScore;
+        }
+
+        return score;
+    }
+}
